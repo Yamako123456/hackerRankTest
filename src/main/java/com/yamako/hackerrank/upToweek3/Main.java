b9c6c70ec02371e7ea1215110ebf1dec547f9ffd
yamako <yamakodev@gmail.com>
2024-05-20 19:41:41 -0700

    upto ZigZag

--- src/main/java/com/yamako/hackerrank/upToweek3/Main.java
+++ src/main/java/com/yamako/hackerrank/upToweek3/Main.java
@@ -14,14 +14,17 @@ public class Main {
         String str1 = "10101";
         String str2 = "00101";
 
-        List<Integer> A = Arrays.asList(1, 1, 1, 2, 3, 5);
+        int[] a  = new int[]{ 2, 3, 5, 1, 4};
+        int n = a.length;
+
+        List<Integer> A = Arrays.asList(41, 10, 1, 2, 3, 5);
 //        System.out.format("A = %s\n", A.toString());
 
 //        List<Integer> B = Arrays.asList(7, 8, 9);
 //        System.out.format("B = %s\n", B.toString());
 
 //        System.out.println(Result_MigratoryBirds.migratoryBirds(A));
-        System.out.println(Result_MaximjmPerimeterTriangle.maximumPerimeterTriangle(A));
+        Result_ZigZagSequence.findZigZagSequence(a, n);
 
         System.out.println();
     }
---
+++ src/main/java/com/yamako/hackerrank/upToweek3/Result_DrawingBook.java
@@ -0,0 +1,10 @@
+package com.yamako.hackerrank.upToweek3;
+
+public class Result_DrawingBook {
+    public static int pageCount(int n, int p) {
+        // Write your code here
+        int  result =  0;
+
+        return result;
+    }
+}
